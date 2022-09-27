93958135e523e255b68f80cf8ad9604b2a41cae6
LaureanoAlonso <laureanoalonso1987@gmail.com>
2022-09-27 13:18:30 -0300

    Ejercicio 1 de Java Basico, donde se crea paquete, clase, metodo y tipos de datos.

---
+++ src/comEjercicio1/Ejercicio1.java
@@ -0,0 +1,38 @@
+package comEjercicio1;
+
+/**
+ * Para este primer ejercicio tendréis que realizar lo siguiente:
+ *
+ * Crea un proyecto de Java desde 0
+ *
+ * Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+ *
+ * Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+ *
+ * Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
+ */
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        int variable1 = 10;
+        long variable2 = 20;
+        double variable3 = 30.5;
+        boolean variable4 = true;
+        boolean variable5 = false;
+        String variable6 = "Hello World";
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+
+        System.out.println(
+                "int " + variable1 + " long " + variable2 + " double " + variable3 + " boolean V " + variable4 + " booleam F " + variable5 + " String " + variable6);
+
+    }
+
+}
